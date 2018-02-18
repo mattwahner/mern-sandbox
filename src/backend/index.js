bc5ef20b519b9de1466e1f1f6f074a816f2b9cb5
Matthew Wahner <32378921+mattwahner@users.noreply.github.com>
2018-02-18 14:50:27 -0600

    Update index.js
    Got rid of debug

--- src/backend/index.js
+++ src/backend/index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 8080;
 app.use(express.static("public"));
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(process.cwd(), "public", "index.html"));
-    console.log("sending stuff");
 });
 
 app.listen(8080, () => console.log(`listening on localhost:${port}`));
